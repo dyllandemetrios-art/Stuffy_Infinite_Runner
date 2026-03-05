43bc69908117a7231ed299d488c4c903ebd447f3
Dylan Boiteux <dyllandemetrios@gmail.com>
2026-03-05 11:11:32 +0100

    coaching m3 s3 part 1
    instantiate n destroy chunks + remove useless assets

--- Assets/Component/ObstacleGenerator/Scripts/ChunkController.cs
+++ Assets/Component/ObstacleGenerator/Scripts/ChunkController.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
-using UnityEngine;
-
 public class ChunkController : MonoBehaviour
 {
     [SerializeField] private Transform _endAnchor;
     
     public Vector3 EndAnchor => _endAnchor.position;
+
+    public bool IsBehindPlayer()
+    {
+        return EndAnchor.z <= 0;
+    }
 }
--- Assets/Component/ObstacleGenerator/Scripts/ObstacleController.cs
+++ Assets/Component/ObstacleGenerator/Scripts/ObstacleController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -8,11 +7,12 @@ public class ObstacleController : MonoBehaviour
     [Header("Parameters")]
     [SerializeField, Tooltip("Translation speed of chunks in m/s")] private float _translationSpeed = 1f;
     [SerializeField] private int _activeChunkCount = 5;
+    [SerializeField] private int _behindChunkCount = 1;
     
     [Header("Components")]
     [SerializeField] private ChunkController[] _chunksPool;
     
-    private List<ChunkController> _instancedChunks = new();
+    private readonly List<ChunkController> _instancedChunks = new();
     
     private void Start()
     {
@@ -21,9 +21,49 @@ public class ObstacleController : MonoBehaviour
 
     private void Update()
     {
-        
+        foreach (var chunk in _instancedChunks)
+        {
+            chunk.transform.Translate(Vector3.back * _translationSpeed * Time.deltaTime);
+        }
+
+        UpdateChunks();
     }
-    
+
+    private void UpdateChunks()
+    {
+        List<ChunkController> behindChunks = new();
+
+        foreach (var chunk in _instancedChunks)
+        {
+            if (chunk.IsBehindPlayer())
+            {
+                behindChunks.Add(chunk);
+            }
+        }
+
+        // Delete potential chunks behind player.
+        if (behindChunks.Count > _behindChunkCount)
+        {
+            int chunkToDeleteCount = behindChunks.Count - _behindChunkCount;
+
+            for (int i = 0; i < chunkToDeleteCount; i++)
+            {
+                var chunkToDelete = behindChunks[i];
+                _instancedChunks.Remove(chunkToDelete);
+                
+                Destroy(chunkToDelete.gameObject);
+            }
+        }
+        
+        // Add potential new chunks.
+        int missingChunkCount = _activeChunkCount - _instancedChunks.Count;
+        for (int i = 0; i < missingChunkCount; i++)
+        {
+            var chunk = AddChunk(LastActiveChunk().EndAnchor);
+            _instancedChunks.Add(chunk);
+        }
+    }
+
     private void AddBaseChunk()
     {
         for (int i = 0; i < _activeChunkCount; i++)
@@ -35,7 +75,7 @@ public class ObstacleController : MonoBehaviour
                 continue;
             }
 
-            var chunk = AddChunk(LastChunk().EndAnchor);
+            var chunk = AddChunk(LastActiveChunk().EndAnchor);
             _instancedChunks.Add(chunk);
         }
     }
@@ -54,7 +94,7 @@ public class ObstacleController : MonoBehaviour
         return chunk;
     }
     
-    private ChunkController LastChunk()
+    private ChunkController LastActiveChunk()
     {
         return _instancedChunks[_instancedChunks.Count - 1];
     }
